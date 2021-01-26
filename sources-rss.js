e1189061d565983a2058d5fb1684edd224321828
Matías Hernández Arellano <matiasfh@gmail.com>
2021-01-25 23:56:32 -0300

    Update sources-rss.js

--- sources-rss.js
+++ sources-rss.js
@@ -1,18 +1,18 @@
 const rss = [
   {
-    title: 'Colby Fayock',
-    url: 'https://www.colbyfayock.com/rss.xml',
+    title: 'Matias Hernandez',
+    url: 'https://www.matiashernandez.dev/rss.xml',
     filters: {
       after: 1546318800000
     }
   },
   {
     title: 'freeCodeCamp News',
-    url: 'https://www.freecodecamp.org/news/author/colbyfayock/rss/'
+    url: 'http://freecodecamp.org/espanol/news/author/matias-hernandez/rss'
   },
   {
-    title: 'YouTube',
-    url: 'https://www.youtube.com/feeds/videos.xml?channel_id=UC7Wpv0Aft4NPNhHWW_JC4GQ'
+    title: 'freeCodeCamp English',
+    url: 'https://www.freecodecamp.org/news/author/matias-hernandez/rss/'
   },
 ];
 
