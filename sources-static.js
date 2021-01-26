27491b31ca6fa58ee49eee3812e00dcb00412a47
Matías Hernández Arellano <matiasfh@gmail.com>
2021-01-26 00:11:26 -0300

    Update sources-static.js

--- sources-static.js
+++ sources-static.js
@@ -1,58 +1,122 @@
 const static = [
   {
-    "title": "Create a New Github Action to Automate Code Tasks with Javascript",
-    "link": "https://egghead.io/playlists/create-a-new-github-action-to-automate-code-tasks-with-javascript-f1e9?af=atzgap",
+    "title": "Course: Creating a gatsby source plugin",
+    "link": "https://egghead.io/playlists/creating-a-gatsby-source-plugin-3f01?af=4cexzz",
     "source": {
       "title": "egghead.io",
-      "link": "https://egghead.io/instructors/colby-fayock?af=atzgap"
+      "link": "https://egghead.io/instructors/matias-hernandez?af=4cexzz"
     }
   },
   {
-    "title": "Create an eCommerce Store with Next.js and Stripe Checkout",
-    "link": "https://egghead.io/playlists/create-an-ecommerce-store-with-next-js-and-stripe-checkout-562c?af=atzgap",
+    "title": "Curso: Crea un plugin de gatsby desde cero",
+    "link": "https://egghead.io/playlists/creacion-de-un-plugin-de-gatsby-desde-cero-5c8b?af=4cexzz",
     "source": {
       "title": "egghead.io",
-      "link": "https://egghead.io/instructors/colby-fayock?af=atzgap"
+      "link": "https://egghead.io/instructors/matias-hernandez?af=4cexzz"
     }
   },
   {
-    "title": "Quickly Create a New Company Website Managed by a CMS with TakeShape and Gatsby",
-    "link": "https://egghead.io/playlists/quickly-create-a-new-company-website-managed-by-a-cms-with-takeshape-and-gatsby-4e4d?af=atzgap",
+    "title": "Curso: Manipulación de arreglos con Javascript",
+    "link": "https://egghead.io/playlists/manipulacion-de-arreglos-con-javascript-12bd?af=4cexzz",
     "source": {
       "title": "egghead.io",
-      "link": "https://egghead.io/instructors/colby-fayock?af=atzgap"
+      "link": "https://egghead.io/instructors/matias-hernandez?af=4cexzz"
     }
   },
   {
-    "title": "Source and Create Nodes with Data from an API in Gatsby to Create a Pokemon Pokedex",
-    "link": "https://egghead.io/playlists/source-and-create-nodes-with-data-from-an-api-in-gatsby-to-create-a-pokemon-pokedex-5e28?af=atzgap",
+    "title": "Curso: Construye componentes avanzados con React hook y patrones de diseño",
+    "link": "https://egghead.io/playlists/hooks-3d62?af=4cexzz",
     "source": {
       "title": "egghead.io",
-      "link": "https://egghead.io/instructors/colby-fayock?af=atzgap"
+      "link": "https://egghead.io/instructors/matias-hernandez?af=4cexzz"
     }
   },
   {
-    "title": "Create, Manage, and Test API Requests with Postman",
-    "link": "https://egghead.io/playlists/create-manage-and-test-api-requests-with-postman-de1b?af=atzgap",
+    "title": "Course: Build advanced components with React hooks and design patterns",
+    "link": "https://egghead.io/playlists/build-advanced-component-with-react-hooks-cd6a?af=4cexzz",
     "source": {
       "title": "egghead.io",
-      "link": "https://egghead.io/instructors/colby-fayock?af=atzgap"
+      "link": "https://egghead.io/instructors/matias-hernandez?af=4cexzz"
     }
   },
   {
-    "title": "Mapping with React Leaflet v2",
-    "link": "https://egghead.io/playlists/mapping-with-react-leaflet-e0e0?af=atzgap",
+    "title": "Encadenamiento opcional en javascript",
+    "link": "https://egghead.io/lessons/javascript-encadenamiento-opcional-en-javascript?af=4cexzz",
     "source": {
       "title": "egghead.io",
-      "link": "https://egghead.io/instructors/colby-fayock?af=atzgap"
+      "link": "https://egghead.io/instructors/matias-hernandez?af=4cexzz"
     }
   },
   {
-    "title": "Build Maps with React Leaflet v2",
-    "link": "https://egghead.io/courses/build-maps-with-react-leaflet?af=atzgap",
+    "title": "Operador nullish coalescing",
+    "link": "https://egghead.io/lessons/javascript-operador-fusion-nula-nullish-coalescing-en-javascript?af=4cexzz",
     "source": {
       "title": "egghead.io",
-      "link": "https://egghead.io/instructors/colby-fayock?af=atzgap"
+      "link": "https://egghead.io/instructors/matias-hernandez?af=4cexzz"
+    }
+  },
+  {
+    "title": "4 formas de llenar un arreglo en javascript",
+    "link": "https://egghead.io/lessons/javascript-4-formas-de-llenar-un-arreglo-en-javascript?af=4cexzz",
+    "source": {
+      "title": "egghead.io",
+      "link": "https://egghead.io/instructors/matias-hernandez?af=4cexzz"
+    }
+  },
+  {
+    "title": "4 ways to populate an array with Javascript",
+    "link": "https://egghead.io/lessons/javascript-4-ways-to-populate-an-array-in-javacript?af=4cexzz",
+    "source": {
+      "title": "egghead.io",
+      "link": "https://egghead.io/instructors/matias-hernandez?af=4cexzz"
+    }
+  },
+  {
+    "title": "3 formas de actualizar un arreglo de objectos en Javascript",
+    "link": "https://egghead.io/lessons/javascript-3-formas-de-actualizar-un-arreglo-de-objetos-en-javascript?af=4cexzz",
+    "source": {
+      "title": "egghead.io",
+      "link": "https://egghead.io/instructors/matias-hernandez?af=4cexzz"
+    }
+  },
+  {
+    "title": "3 ways to update the content of an array of objects with Javascript",
+    "link": "https://egghead.io/lessons/javascript-3-ways-to-update-the-content-of-an-array-of-objects-with-javascript?af=4cexzz",
+    "source": {
+      "title": "egghead.io",
+      "link": "https://egghead.io/instructors/matias-hernandez?af=4cexzz"
+    }
+  },
+  {
+    "title": "Personaliza tu perfil de github con un Readme personalizado",
+    "link": "https://egghead.io/lessons/egghead-personaliza-tu-perfil-en-github-con-el-nuevo-perfil-readme?af=4cexzz",
+    "source": {
+      "title": "egghead.io",
+      "link": "https://egghead.io/instructors/matias-hernandez?af=4cexzz"
+    }
+  },
+  {
+    "title": "Como eliminar elementos en un arreglo",
+    "link": "https://egghead.io/lessons/javascript-como-eliminar-elementos-dentro-de-un-arreglo?af=4cexzz",
+    "source": {
+      "title": "egghead.io",
+      "link": "https://egghead.io/instructors/matias-hernandez?af=4cexzz"
+    }
+  },
+  {
+    "title": "4 ways to remove duplicate elements from an array",
+    "link": "https://egghead.io/lessons/javascript-4-ways-to-remove-duplicates-elements-from-an-array-with-javascript?af=4cexzz",
+    "source": {
+      "title": "egghead.io",
+      "link": "https://egghead.io/instructors/matias-hernandez?af=4cexzz"
+    }
+  },
+  {
+    "title": "Crear animaciones con React Native y la API Animated",
+    "link": "https://egghead.io/lessons/react-native-crear-animacion-con-react-native-y-la-api-animated?af=4cexzz",
+    "source": {
+      "title": "egghead.io",
+      "link": "https://egghead.io/instructors/matias-hernandez?af=4cexzz"
     }
   }
 ]
